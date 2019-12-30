50233c206d2060aefec206b56e2d665bf6540154
amantham20 <48414198+amantham20@users.noreply.github.com>
2019-12-29 22:42:18 -0500

    Revert "Reverty"
    This reverts commit 56a97352e0987eafb08b5f5125b1f8dfb81c6951.

---
+++ js/transition.js
@@ -0,0 +1,18 @@
+import Highway from '@dogstudio/highway';
+import { TimelineLite } from 'gsap';
+
+class Fade extends Highway.Transition { in ({ from, to, done }) {
+        const t1 = new TimelineLite();
+        t1.fromTo(to, 0.5, { left: '-100%', top: '50%' }, { left: '0%' })
+            .fromTo(to, 0.5, { height: '2vh' }, {
+                height: '90vh',
+                top: '10%',
+                onComplete: function() {
+                    done();
+                }
+            });
+    }
+    out({ from, done }) {
+        done();
+    }
+}
